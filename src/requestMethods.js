18dd30a9a578ebbb22769193a0345156fe8b8b46
WenjingYao <18811303016@163.com>
2023-08-08 14:11:47 +0100

    api route change

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://lidl-shopping-website-api.onrender.com";
+const BASE_URL = "https://lidl-shopping-website-api.onrender.com/api/";
 // const TOKEN = "";
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
